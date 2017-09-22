f27f2bfc7db01c93d8ff42751d146e8b52d54de0
unknown <luca.canizzo@gmail.com>
2017-09-22 16:54:33 -0400

    Added key.js and updated with twitter keys

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'yox5XGaNKWeiJdnxgY5RUSwQf',
+  consumer_secret: 'b3ppv9ZBbj0YpUNCXEYuFEK9Lse9Iaepd9TCAHG3Gg2fVQ5K11',
+  access_token_key: '911328041879601153-TIPP8vGBtLslTZFocHJsoY9FOuA9ARu',
+  access_token_secret: 'kiKLkyoKwJKtdIsvhgHkahWagPxuGaAzU9Nem5J9WJT7v',
+}
+
+module.exports = twitterKeys;
