1a8317258cb8416ded3b7a57616ff057f16fec30
unknown <luca.canizzo@gmail.com>
2017-09-22 19:05:30 -0400

    Progressing through spotify logic

--- key.js
+++ key.js
@@ -5,9 +5,4 @@ module.exports = {
         access_token_key: '911328041879601153-TIPP8vGBtLslTZFocHJsoY9FOuA9ARu',
         access_token_secret: 'kiKLkyoKwJKtdIsvhgHkahWagPxuGaAzU9Nem5J9WJT7v',
     },
-    
-    spotifyKeys: {
-        client_id: "fac328254777437799105768aead360a",
-        client_secret: "233cb2cb159c4704878f2ff70d7beecf",
-    }
 };
--- siri-who.js
+++ siri-who.js
@@ -1,10 +1,14 @@
-// K E Y S
+// R E Q U I R E S
 const keys = require('./key.js');
+const twitter = require('twitter');
+const spotify = require('spotify');
+const request = require('request');
+
+// K E Y S 
 const twitterKeys = keys.twitterKeys;
 const spotifyKeys = keys.spotifyKeys;
         
 //  T W I T T E R 
-const twitter = require('twitter');
 var client = new twitter(twitterKeys);
 // var params = {screen_name: 'nodejs'};
 function printTweets() {
@@ -16,12 +20,33 @@ function printTweets() {
                    tweets[i].user.screen_name, 'said: \n', 
                    "  ", tweets[i].text, '\n');    
           }
+        } else {
+            console.log('E R R O R: ', error);
         }
     });
 }
 
+// S P O T I F Y 
+let track = process.argv[3];
+//One word only, need to get string of words for titles
+function spotifyThis(){
+    request('https://api.spotify.com/v1/search?q=' + track + '&type=track', function(err, data){
+        if (!err) {
+            console.log(data);
+            console.log(
+                'Artist(s): ', data +
+                'Title: ', data +
+                'Listen: ', data +
+                'Album: ', data
+            )
+        } else {
+            console.log('E R R O R: ', err);            
+        }
+    })
+}
+
 // S W I T C H
-if (process.argv.length !==3){
+if (process.argv.length == 2){
     console.log('Help: \n');
 } else {
     switch (process.argv[2]){
@@ -29,13 +54,13 @@ if (process.argv.length !==3){
             printTweets();
             break;
         case "spotify-this-song":
-            printTweets();
+            spotifyThis();
             break;
         case "movie-this":
-            printTweets();
+            movieThis();
             break;
         case "do-what-it-says":
-            printTweets();
+            listenToThis();
             break;
         default:
             console.log('no valid input (input options)');
