f9a34ecb6c2d81d717de077cd9311fe018286d3a
unknown <luca.canizzo@gmail.com>
2017-09-25 18:50:21 -0400

    Updated weather output format

--- siri-who.js
+++ siri-who.js
@@ -13,41 +13,43 @@ const spotifyKeys = keys.spotifyKeys;
 // D E F I N E  I N P U T 
 var input = "";
 if (process.argv.length > 3) {
-    for (let i = 3; i < process.argv.length; i++){
-        input += (process.argv[i]+'+');
+    for (let i = 3; i < process.argv.length; i++) {
+        input += (process.argv[i] + '+');
     }
+}
+
+// H E L P  
+function help() {
+    console.log('Instructions: \n');
 }
 
 // D E F I N E   A D D R E S S 
 var address = [];
 var string = "";
 
-for (let i = 3; i < process.argv.length; i++){
+for (let i = 3; i < process.argv.length; i++) {
     address.push(process.argv[i]);
 }
-string = address.toString();   
-
-// H E L P  
-function help(){
-    console.log('Instructions: \n');
-}
+string = address.toString();
 
 // W E A T H E R
 function weatherHere() {
-    weather.find({search: string}, function(err, data){
-        if(!err){
+    weather.find({
+        search: string
+    }, function (err, data) {
+        if (!err) {
             console.log('\n ***Future Forecast***\n',
-            string);
-            for (let i = 0; i < data[0].forecast.length; i++){
+                string);
+            for (let i = 0; i < data[0].forecast.length; i++) {
                 var precip = data[0].forecast[i].precip;
-                if (!precip){
+                if (!precip) {
                     precip = 0;
                 }
                 console.log('\n', data[0].forecast[i].day + ':\n',
-                ' -High:', '\n   ',data[0].forecast[i].high,
-                '\n  -Low:', '\n   ', data[0].forecast[i].low,
-                '\n  -Precipitation:', '\n   ', precip + '%',
-                '\n  -Cloud Cover:', '\n   ', data[0].forecast[i].skytextday);
+                    ' -High:', '\n   ', data[0].forecast[i].high,
+                    '\n  -Low:', '\n   ', data[0].forecast[i].low,
+                    '\n  -Precipitation:', '\n   ', precip + '%',
+                    '\n  -Cloud Cover:', '\n   ', data[0].forecast[i].skytextday);
             }
         } else {
             console.log('error: \n', err);
@@ -62,7 +64,7 @@ function printTweets() {
     clientTwitter.get('statuses/user_timeline', function (error, tweets, response) {
         if (!error) {
             for (let i = 0; i < tweets.length; i++) {
-                let text = '"'+tweets[i].text+'"';                
+                let text = '"' + tweets[i].text + '"';
                 console.log(
                     tweets[i].created_at, '\n',
                     tweets[i].user.screen_name, 'said: \n',
@@ -79,49 +81,48 @@ var spotifyApi = new SpotifyWebApi(spotifyKeys);
 
 function spotifyThis(input) {
     spotifyApi.clientCredentialsGrant()
-    .then(function (data) {
-        spotifyApi.setAccessToken(data.body['access_token']);
-        spotifyApi.searchTracks(input)
-            .then(function (data) {
-                // console.log('\n D A T A: \n', data.body.tracks.items[0]);
-                console.log('\n ***Song Info*** \n\n',
-                    ' -Artist:', '\n   ', data.body.tracks.items[0].artists[0].name,
-                    '\n  -Title:', '\n   ', data.body.tracks.items[0].name,
-                    '\n  -Preview:', '\n   ', data.body.tracks.items[0].preview_url,
-                    '\n  -Album:', '\n   ', data.body.tracks.items[0].album.name,
-                    '\n  -Listen:', '\n   ', data.body.tracks.items[0].external_urls.spotify
-            )
-            }, function (err) {
-                console.log(err);
-            })
-    }, function (err) {
-        console.log('Could not get access token', err);
-    })
+        .then(function (data) {
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.searchTracks(input)
+                .then(function (data) {
+                    console.log('\n ***Song Info*** \n\n',
+                        ' -Artist:', '\n   ', data.body.tracks.items[0].artists[0].name,
+                        '\n  -Title:', '\n   ', data.body.tracks.items[0].name,
+                        '\n  -Preview:', '\n   ', data.body.tracks.items[0].preview_url,
+                        '\n  -Album:', '\n   ', data.body.tracks.items[0].album.name,
+                        '\n  -Listen:', '\n   ', data.body.tracks.items[0].external_urls.spotify
+                    )
+                }, function (err) {
+                    console.log(err);
+                })
+        }, function (err) {
+            console.log('Could not get access token', err);
+        })
 }
 
 // O M D B  
 function movieThis(input) {
     if (process.argv[2] !== "do-it" && !process.argv[3]) {
         input = 'It';
-    } 
+    }
     let queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=40e9cece";
-    
-    request(queryUrl, function(err, data){
-        if (err){
-          throw err;
+
+    request(queryUrl, function (err, data) {
+        if (err) {
+            throw err;
         } else {
-          let object = JSON.parse(data.body);
-          console.log('\n ***Movie Info*** \n\n',
-          '-Title: ', '\n  ', object.Title, 
-          '\n -Year: ', '\n  ', object.Year, 
-          '\n -IMDB Rating: ', '\n  ', object.imdbRating,
-          '\n -Rotten Tomatoes Rating: ', '\n  ', object.Ratings[1].Value,
-          '\n -Country: ', '\n  ', object.Country,
-          '\n -Language: ', '\n  ', object.Language,
-          '\n -Plot: ', '\n  ', object.Plot,
-          '\n -Actors: ', '\n  ', object.Actors)
+            let object = JSON.parse(data.body);
+            console.log('\n ***Movie Info*** \n\n',
+                '-Title: ', '\n  ', object.Title,
+                '\n -Year: ', '\n  ', object.Year,
+                '\n -IMDB Rating: ', '\n  ', object.imdbRating,
+                '\n -Rotten Tomatoes Rating: ', '\n  ', object.Ratings[1].Value,
+                '\n -Country: ', '\n  ', object.Country,
+                '\n -Language: ', '\n  ', object.Language,
+                '\n -Plot: ', '\n  ', object.Plot,
+                '\n -Actors: ', '\n  ', object.Actors)
         }
-      })
+    })
 }
 
 // S W I T C H   F U N C T I O N S
@@ -135,7 +136,7 @@ function switchFunctions(argument, input) {
         case "spotify":
         case "spotify-this":
         case "-s":
-        case "song":        
+        case "song":
             spotifyThis(input);
             break;
         case "movie":
@@ -155,23 +156,20 @@ function switchFunctions(argument, input) {
     }
 }
 
-
 // D O   I T   F U N C T I O N 
-
-function listenToThis(){
+function listenToThis() {
     var doThis = [];
-    fs.readFile('random.txt', 'utf8', function(error, data) {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
         if (error) {
             return console.log(error);
         } else {
             doThis = (data.split(","));
-            let input = doThis[1];          
+            let input = doThis[1];
             let prompt = doThis[0];
             switchFunctions(prompt, input);
-        }       
+        }
     })
 }
-
 
 // S W I T C H
 if (process.argv.length == 2) {
